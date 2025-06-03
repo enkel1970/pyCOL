58b9bbb98c990fd08b72d741656ad295cd3129cb
enkel1970 <65807899+enkel1970@users.noreply.github.com>
2025-06-03 18:57:33 +0200

    Add comment
    add some comment to code

--- camera.py
+++ camera.py
@@ -7,27 +7,29 @@ def find_camera_index_by_name_substring():
     Find index of camera ocal2: ocal2.
     """
     platform_name = platform.system().lower()
-    target_name = "ocal2: ocal2"
+    target_name_linux = "ocal2: ocal2"
+    target_name_windows = "ocal2"
+    target_name_darwin = "ocal2: ocal2"
 
     if platform_name == 'linux':
         print('Using V4L2 backend for Linux cameras.')
         for camera_info in enumerate_cameras(cv2.CAP_V4L2):
             print(f'{camera_info.index}: {camera_info.name}')
-            if camera_info.name == target_name:
+            if camera_info.name == target_name_linux:
                 print(f'Found camera with name "ocal2" at index {camera_info.index}')
-                return camera_info.index
+                return camera_info.index - 1  # Adjusting index for V4L2 backend
     elif platform_name == 'windows':
         print('Using DirectShow backend for Windows cameras.')
         for camera_info in enumerate_cameras(cv2.CAP_DSHOW):
             print(f'{camera_info.index}: {camera_info.name}')
-            if camera_info.name == target_name:
+            if camera_info.name == target_name_windows:
                 print(f'Found camera with name "ocal2" at index {camera_info.index}')
                 return camera_info.index
     elif platform_name == 'darwin':
         print('Using AVFoundation backend for macOS cameras.')
         for camera_info in enumerate_cameras(cv2.CAP_AVFOUNDATION):
             print(f'{camera_info.index}: {camera_info.name}')
-            if camera_info.name == target_name:
+            if camera_info.name == target_name_darwin:
                 print(f'Found camera with name "ocal2" at index {camera_info.index}')
                 return camera_info.index
     else:
